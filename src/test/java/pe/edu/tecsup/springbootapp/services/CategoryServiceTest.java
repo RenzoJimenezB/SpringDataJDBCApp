97bb94ef4238f07a29c430941456bf45275d7c46
RenzoJimenezB <adrianrjb@outlook.es>
2024-11-23 13:10:35 -0500

    CategoryServiceTest implementation

--- src/test/java/pe/edu/tecsup/springbootapp/services/CategoryServiceTest.java
+++ src/test/java/pe/edu/tecsup/springbootapp/services/CategoryServiceTest.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pe.edu.tecsup.springbootapp.entities.Category;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +21,14 @@ class CategoryServiceTest {
     @Test
     void findAll() throws Exception {
         log.info("Testing CategoryService.findAll()");
-        categoryService.findAll();
+
+        List<Category> categories = categoryService.findAll();
+
+        assertNotNull(categories);
+        assertFalse(categories.isEmpty());
+        assertFalse(categories.contains(null));
+        
+        categories.forEach(category -> System.out.println(category.getName()));
+        categories.forEach(System.out::println);
     }
 }
